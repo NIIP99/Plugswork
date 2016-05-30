3d86b2395b1e1a132393888dea48f9dc4bcf6e5c
legoboy0215 <legoboy0215@users.noreply.github.com>
2016-05-31 06:24:18 +0800

    Removed Server::getInstance

--- PHP-Build/src/Plugswork/PwListener.php
+++ PHP-Build/src/Plugswork/PwListener.php
@@ -11,7 +11,6 @@
 
 namespace Plugswork;
 
-use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerChatEvent;
@@ -24,7 +23,7 @@ class PwListener implements Listener{
     private $plugin;
     
     public function __construct(Plugswork $plugin){
-	Server::getInstance()->getPluginManager()->registerEvents($this, $plugin);
+	$plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
         $this->plugin = $plugin;
     }
     
