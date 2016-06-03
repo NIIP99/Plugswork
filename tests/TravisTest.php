b103c8ad961b0e827f2c7feb1778b7881312fe37
Keith <keithkfng@users.noreply.github.com>
2016-06-03 10:42:27 +1000

    ???

--- tests/TravisTest.php
+++ tests/TravisTest.php
@@ -6,20 +6,24 @@ $server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --d
 	2 => ["pipe", "w"]
 ], $pipes);
 
+if(!is_resource($server)){
+	die('Failed to create process');
+}
+
 fwrite($pipes[0], "version\nmakeplugin Plugswork\nstop\n\n");
+fclose($pipes[0]);
 
 while(!feof($pipes[1])){
 	echo fgets($pipes[1]);
 }
 
-fclose($pipes[0]);
 fclose($pipes[1]);
 fclose($pipes[2]);
 
 echo "\n\nReturn value: ". proc_close($server) ."\n";
 
 if(count(glob("plugins/DevTools/Plugswork*.phar")) === 0){
-	echo "No server phar created!\n";
+	echo "No Plugswork phar created!\n";
 	exit(1);
 }else{
 	exit(0);
